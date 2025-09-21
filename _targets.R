733cade1973adcdd0eae031a2ead8fac599419e3
Arun <akri0026@monash.student.edu>
2025-09-21 12:56:46 +1000

    ADDED: Fetched and combined all the necessary data

--- _targets.R
+++ _targets.R
@@ -8,7 +8,7 @@ tar_option_set(packages = c(
 
 source("R/utils_registry.R")
 source("R/utils_fetch_abs.R")
-#source("R/utils_fetch_rba.R")
+source("R/utils_fetch_rba.R")
 #source("R/utils_clean_transform.R")
 
 list(
@@ -17,7 +17,16 @@ list(
   tar_target(series_registry, read_registry(registry_file)),
   
   # Phase 1: LOAD
-  tar_target(p1_abs_raw, fetch_abs_by_registry(series_registry)),
-  # Phase 1.2: Load local ABS series
-  tar_target(p1_abs_bad_ids, find_bad_ids(series_registry, p1_abs_raw))
+  # Phase 1.1: Load  ABS series
+  tar_target(p11_abs_raw, fetch_abs_by_registry(series_registry)),
+  #tar_target(p11_abs_bad, find_bad_abs_ids(series_registry, p11_abs_raw)),
+  # Phase 1.2: Load local RBA series
+  tar_target(p12_rba_raw, fetch_rba_by_registry(series_registry)),
+  # tar_target(p12_rba_bad, find_bad_rba_ids(series_registry, p12_rba_raw))
+  # Combine both ABS and RBA data object
+  tar_target(
+    p1_abs_rba_raw, 
+    bind_rows(p11_abs_raw, p12_rba_raw) |> 
+               arrange(date, series_id)
+    )
 )
